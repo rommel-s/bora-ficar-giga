5c1839158848bba1f3224f58bded3fb5118e623e
Rommel Santos <rommelrenne@gmail.com>
2026-01-03 00:38:21 -0300

    Start changing workout icons

--- components/Workout-placeholder/index.jsx
+++ components/Workout-placeholder/index.jsx
@@ -1,9 +1,19 @@
+import Image from "next/image";
+
 import "./styles.css";
 
 const WorkoutPlaceholder = ({ name, weight, sets, img }) => {
   return (
     <main className="workout-placeholder">
-      <div className="imgplace"></div>
+      <div className="imgplace">
+        <Image
+          src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExdjYwcDdlNGl3Zm00a3N0dGs5aHgydHh2cG9naDNwZGw4cGhpdmt5NiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/kDQAwPe2owJrONdtUE/giphy.gif"
+          alt="Minha animação"
+          width="100"
+          height="100"
+          unoptimized
+        />
+      </div>
       <div className="workout-info">
         <h2 className="workout-name">{name}</h2>
         <h4 className="workout-details">
