cc2356287375213b177421299a29559160f88291
Rommel Santos <rommelrenne@gmail.com>
2026-01-03 02:53:04 -0300

    Added gist path

---
+++ public/api.js
@@ -0,0 +1,4 @@
+const urlGist =
+  "https://gist.githubusercontent.com/rommel-s/db2bd37cb484d4f5b78a1eae48502521/raw/38681b38caf03ed0c81ac17b05daec1e9f9a12a4/treinos.json";
+
+export default urlGist;
