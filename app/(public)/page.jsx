c9846ab5970f4410446ebdac67a7a9d9b15370ab
Rommel Santos <rommelrenne@gmail.com>
2026-01-03 00:36:23 -0300

    Solved scroll pages bug

--- app/(public)/page.jsx
+++ app/(public)/page.jsx
@@ -1,9 +1,20 @@
+"use client";
+
 import { WorkoutButton } from "@/components/Buttons";
 import styles from "./page.module.css";
 import Link from "next/link";
 import { MainTitle, SubtitleText } from "@/components/Titles";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    document.body.style.overflowY = "hidden";
+
+    return () => {
+      document.body.style.overflowY = "auto";
+    };
+  }, []);
+
   return (
     <div className={styles.container}>
       <div className={styles.titlecontainer}>
