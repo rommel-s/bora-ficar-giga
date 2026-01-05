041e804f53f74d89fb8b0b36947019592f8d8fad
Rommel Santos <rommelrenne@gmail.com>
2026-01-05 00:49:54 -0300

    Added workouts lists

--- public/api.js
+++ public/api.js
@@ -1,4 +1,4 @@
 const urlGist =
-  "https://gist.githubusercontent.com/rommel-s/db2bd37cb484d4f5b78a1eae48502521/raw/38681b38caf03ed0c81ac17b05daec1e9f9a12a4/treinos.json";
+  "https://gist.githubusercontent.com/rommel-s/db2bd37cb484d4f5b78a1eae48502521/raw/78d118c9f4327a5a0929c54a869636e906a45401/treinos.json";
 
 export default urlGist;
