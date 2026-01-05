b1b10e6c2d4eaadc54051daff69f690ddafa8bcc
Rommel Santos <rommelrenne@gmail.com>
2026-01-05 00:52:10 -0300

    Changed checkboxes behavior

--- components/Workout-placeholder/index.jsx
+++ components/Workout-placeholder/index.jsx
@@ -7,7 +7,7 @@ const WorkoutPlaceholder = ({ name, weight, sets, img }) => {
     <main className="workout-placeholder">
       <div className="imgplace">
         <Image
-          src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExdjYwcDdlNGl3Zm00a3N0dGs5aHgydHh2cG9naDNwZGw4cGhpdmt5NiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/kDQAwPe2owJrONdtUE/giphy.gif"
+          src={img || null}
           alt="Minha animação"
           width="100"
           height="100"
